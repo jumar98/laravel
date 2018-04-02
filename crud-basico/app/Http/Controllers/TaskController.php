ed904c961a78215caed6aa62664fce2dae8f7ef1
Julian Martinez <jumar98@hotmail.com>
2018-04-01 21:57:45 -0500

    Añadimos los metodos del CRUD

---
+++ crud-basico/app/Http/Controllers/TaskController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+     $tasks = Task::orderBy('id','DESC')->paginate(3);
+     return view('task.index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('task.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, ['name' => 'required', 'description' => 'required']);
+        Task::create($request->all());
+        return redirect()->route('task.index')->with('success','Registro exitoso');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tasks = Task::find($id);
+        return view('Task.show', compact('tasks'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $task = Task::find($id);
+        return view('Task.edit', compact('tasks'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,[ 'name'=>'required', 'description'=>'required']);
+        Task::find($id)->update($request->all());
+        return redirect()->route('task.index')->with('success','Registro actualizado satisfactoriamente');
+ 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Task::find($id)->delete();
+        return redirect()->route('Task.index')->with('success','Registro eliminado satisfactoriamente');
+    }
+}
