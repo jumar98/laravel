d3af9fadfd6f055f1128477f78bb3222f83bedd1
Julian Martinez <jumar98@hotmail.com>
2018-04-03 21:08:19 -0500

    cambios

--- php-class/clase-sintaxis/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nombre = "Julian";
-$edad = 20;
-$direccion = "VICOL";
-
-echo nl2br("Hola $nombre \n"); // Procesa el contenido
-echo nl2br('Mundo $edad'."\n"); // Envia sin procesar
-echo 'Mi direccion: '.$direccion;
-?>
