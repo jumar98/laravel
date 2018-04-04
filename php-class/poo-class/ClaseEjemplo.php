b910a13dc8afc5fdf5fbff7e4ce4667bbbf881f3
Julian Martinez <jumar98@hotmail.com>
2018-04-03 22:48:16 -0500

    Estructura html.
    Imprimir desde php

--- php-class/poo-class/ClaseEjemplo.php
+++ php-class/poo-class/ClaseEjemplo.php
@@ -1,5 +1,26 @@
 <?php
 
+
+class Html{
+
+    public function head(){
+        echo "<html><head>";
+        //Contenido del head
+        echo "</head>";
+    }
+
+    public function openBody(){
+        echo "<body>";
+    }
+
+    public function closeBody(){
+        echo "</body>";
+    }
+
+    public function foot(){
+        echo "</html>";
+    }
+}
 class ClaseEjemplo{
 
     private $nombre = "Julian"; // Restringe el propiedades para modificacion o acceso 
@@ -18,9 +39,28 @@ class ClaseEjemplo{
     }
 }
 
+$html = new Html();
+$html->head();
+$html->openBody();
 $obj = new ClaseEjemplo();
+$obj2 = new ClaseEjemplo();
+$obj3 = new ClaseEjemplo();
+$obj4 = new ClaseEjemplo();
+$obj5 = new ClaseEjemplo();
 
+$obj->setNombre("Juan");
+$obj2->setNombre("Pepe");
+$obj3->setNombre("Laura");
+$obj4->setNombre("David");
+$obj5->setNombre("Eder");
 
-echo nl2br("Nombre: ".$obj->getNombre()."\n");
-$obj->setNombre("Pedro Juan");
-echo "Nombre: ".$obj->getNombre();
+echo nl2br("1: ".$obj->getNombre()."\n");
+echo nl2br("2: ".$obj2->getNombre()."\n");
+echo nl2br("3: ".$obj3->getNombre()."\n");
+echo nl2br("4: ".$obj4->getNombre()."\n");
+echo "5: ".$obj5->getNombre();
+
+$html->closeBody();
+$html->foot();
+
+?>
