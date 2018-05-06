f6abfcda311c74b9a3ba4f6f0ba53527f3d536d8
Julian Martinez <jumar98@hotmail.com>
2018-05-05 20:25:08 -0500

    Conexion con mysqli, como objeto

--- php-class/php-mysql/index.php
+++ php-class/php-mysql/index.php
@@ -3,3 +3,8 @@ $server = "localhost";
 $usuario = "root";
 $pass = "";
 $nameBD = "php_mysql";
+
+$conexion = new mysqli($server,$usuario,$pass,$nameBD);
+echo "tipo de variable: ".gettype($conexion)."<br>";
+echo "contenido de variable"."<br>";
+var_dump($conexion);
