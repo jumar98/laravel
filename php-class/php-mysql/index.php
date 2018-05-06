a50566a1ef8924509064a2163125498afaf266db
Julian Martinez <jumar98@hotmail.com>
2018-05-05 21:10:59 -0500

    Cambio condicion if

--- php-class/php-mysql/index.php
+++ php-class/php-mysql/index.php
@@ -5,6 +5,12 @@ $pass = "";
 $nameBD = "php_mysql";
 
 $conexion = new mysqli($server,$usuario,$pass,$nameBD);
+
+if($conexion->connect_errno){
+    echo "Falló la conexion";
+}else{
+    echo "Conexion exitosa: ".mysqli_get_host_info($conexion);
+}
 echo "tipo de variable: ".gettype($conexion)."<br>";
 echo "contenido de variable"."<br>";
 var_dump($conexion);
