3271d0f667e864a84c435fbfecdd6ea28ba1152e
Julian Martinez <jumar98@hotmail.com>
2018-05-14 22:49:40 -0500

    Mostrar mensajes en la caja.

--- php-ajax/index.php
+++ php-ajax/index.php
@@ -13,11 +13,18 @@ include "conexion.php";
 <body>
     <div id="container">
         <div id="chat_box">
+        <?php
+            $query = "select * from chat order by id desc";
+            $run = $objCon->conexion->query($query);
+
+            while($row = $run->fetch_array()) :
+        ?>
             <div id="chat_data">
-                <span style="color:green">Wali:</span>
-                <span style="color:red">How are you?</span>
-                <span style="color:blue; float:right">12:30PM</span>
+                <span style="color:green"><?php echo $row['name']; ?>:</span>
+                <span style="color:red"><?php echo $row['message']; ?></span>
+                <span style="color:blue; float:right"><?php echo $row['date']; ?></span>
             </div>
+            <?php endwhile; ?>
         </div>
         <form action="post" action="recibir.php">
             <input type="text" placeholder="Nombre" name="name">
